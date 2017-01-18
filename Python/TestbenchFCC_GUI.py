c26d3e42a2f9e0aee2d2c4921f38d78a9899601d
adnan5535 <a.dna.n5535@gmail.com>
2017-01-18 15:48:22 -0700

    Initialize Repo

---
+++ Python/TestbenchFCC_GUI.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 03:40:29 2016
+
+@author: Adnan
+"""
+from Tkinter import *
+from ttk import Button, Style
+
+class TestbenchFCC_GUI(Frame):
+    def __init__(self,parent):
+        print "foo"
+    def main_state_machine(self):
+        print "foo"
+            
+
+
+if __name__ == "__main__":
+    window = Tk()
+    TestbenchFCC_GUI(window)
+    window.mainloop()
